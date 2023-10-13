1caad97049c5832a20342f14b97b742909492944
Erica <erica.squicciarini@gmail.com>
2023-10-13 18:20:41 -0400

    initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.foodtruck.app;
+
+public class FoodTruckApp {
+	private FoodTruck[] fleet = new FoodTruck[5]
+	
+
+	public static void main(String[] args) {
+		FoodTruckApp fta= new FoodTruckApp();
+
+	}
+public void doYourThing() {
+	FoodTruckApp fta = new FoodTruckApp();
+	  fta.doYourThing();
+	  
+//	  prompt user
+//	  write out a menu for user input
+//	  this one will maintain array of food trucks
+	  
+}
+}
